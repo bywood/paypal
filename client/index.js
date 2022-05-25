3c57bda7bc7058bdef3a7f13267720e950f6e91a
bywood <bywood@paypal.com>
2022-05-24 18:35:49 -0700

    [DTNOR-450] Updated to use CardFields.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable consistent-return, new-cap, no-alert, no-console */
-
+/*
 paypal.Buttons({
     createOrder: function (data, actions) {
         return actions.order.create({
@@ -19,3 +19,48 @@ paypal.Buttons({
         console.log('Something went wrong', err);
     }
 }).render('#paypal-button-container'); // Display payment options on your web page
+*/
+
+const cardFields = paypal.CardFields({
+    createOrder: function (data, actions) {
+        console.log("createOrder:", data);
+        return fetch('/api/paypal/order/create/', {
+            method: 'post'
+        }).then((res) => {
+            return res.json();
+        }).then((orderData) => {
+            console.log("order:", orderData);
+            return orderData.id;
+        });
+    },
+    onApprove: function (data, actions) {
+        console.log("onApprove:", data);
+        const { orderID } = data;
+        return fetch(`/api/paypal/order/${orderID}/capture/`, {
+            method: 'post'
+        }).then((res) => {
+            return res.json();
+        }).then((orderData) => {
+            console.log("Payment approved and captured:", orderData);
+        });
+    },
+    onError: function (err) {
+        console.log('Something went wrong', err);
+    },
+    onChange: function (event) {
+        console.log("onChange:", event)
+    }
+});
+
+cardFields.render('#paypal-button-container'); // Display payment options on your web page
+
+const button = document.getElementById("button");
+if (button) {
+    button.addEventListener("click", function () {
+        cardFields.submit().then(() => {
+            console.log("success");
+        }).catch((error) => {
+            console.log("error:", error)
+        });
+    });
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const express = require("express")
+const fetch = require("node-fetch");
 const { resolve } = require("path")
+
+// const CLIENT_ID = 'AVgzJ5j6adTAjZHTRHCgzKdWXwOxaCcUc9tZWsdNWVe9WwNasAKWHbiuluX8nVBksz0hV9psUMGLQuWW'
+const CLIENT_ID = 'alc_client1'
+// const APP_SECRET = 'EDpHan08Jsq1bISzlG8jhU4kvYQHS-pdEGYGpxRnJmm5h2dQv046NMMuCGGzpTIpKBOsQP2I48dfbJlw'
+const APP_SECRET = 'secret'
+const base = "http://localhost.paypal.com:8000";
 
 const app = express()
 
@@ -8,5 +15,53 @@ app.use(express.static(resolve(__dirname, "../client")))
 app.get("/", (req, res) => {
   res.sendFile(resolve(__dirname, "../client/index.html"));
 });
+
+(async function () {
+
+  async function generateAccessToken() {
+    const auth = Buffer.from(CLIENT_ID + ":" + APP_SECRET).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "post",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+    const data = await response.json();
+    return data.access_token;
+  }
+
+  app.post("/api/paypal/order/create/", async (req, res) => {
+    // res.sendFile(resolve(__dirname, "../client/index.html"));
+    // console.log('Request', req)
+    // res.json({id: '123456789'})
+    // paypalSDK.createOrder(res)
+    console.log("Response", res.body)
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: "100.00",
+            },
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    console.log('Data: ', data)
+    res.json(data);
+  });
+
+})();
 
 app.listen(8080)
