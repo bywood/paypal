6c9df14295b1168898977488be5eeb24803b1b94
bywood <bywood@paypal.com>
2022-07-11 16:58:00 -0700

    Added card field examples.

--- client/index.js
+++ client/index.js
@@ -19,3 +19,217 @@ paypal.Buttons({
         console.log('Something went wrong', err);
     }
 }).render('#paypal-button-container'); // Display payment options on your web page
+
+(function () {
+
+    const cardFields = paypal.CardFields({
+        style: {
+            "input": {
+                "color": "blue",
+                "box-shadow": "10px 10px 10px black"
+            },
+            "input.invalid": {
+                "color": "purple"
+            },
+        },
+        //style: {
+        //    "@im\port url(\"https://www.example.com/path/to/css\");/*": {}
+        //},
+        createOrder: function (data, actions) {
+            console.log("createOrder:", data);
+            return fetch('/api/paypal/orders/create/', {
+                method: 'post'
+            }).then((res) => {
+                return res.json();
+            }).then((orderData) => {
+                console.log("order:", orderData);
+                return orderData.id;
+            });
+        },
+        onApprove: function (data, actions) {
+            console.log("onApprove:", data);
+            const { orderID } = data;
+            return fetch(`/api/paypal/orders/${orderID}/capture/`, {
+                method: 'post'
+            }).then((res) => {
+                return res.json();
+            }).then((orderData) => {
+                console.log("Payment approved and captured:", orderData);
+            });
+        },
+        onError: function (err) {
+            console.log('Something went wrong', err);
+        },
+        //onChange: function (event) {
+            //console.log("onChange:", event);
+        //}
+    });
+
+    cardFields.render('#paypal-card-field-container'); // Display payment options on your web page
+
+    const button = document.getElementById("pay-now-button");
+    if (button) {
+        button.addEventListener("click", function () {
+            cardFields.submit().then(() => {
+                console.log("success");
+            }).catch((error) => {
+                console.log("error:", error)
+            });
+        });
+    }
+
+})();
+
+let cardFields = null;
+(function () {
+
+    const cardNameContainer = document.getElementById('card-name-field-container');
+    const cardNumberContainer = document.getElementById('card-number-field-container');
+    const cardCvvContainer = document.getElementById('card-cvv-field-container');
+    const cardExpiryContainer = document.getElementById('card-expiry-field-container');
+    const cardPostalCodeContainer = document.getElementById('card-postal-code-container');
+    const button = document.getElementById('submit-button');
+
+    cardFields = paypal.CardFields({
+
+        createOrder: (data, actions) => {
+
+            // Create the order on your server and return the order id
+
+            return fetch('/api/paypal/order/create/', {
+                method: 'post'
+            }).then((res) => {
+                return res.json();
+            }).then((orderData) => {
+                return orderData.id;
+            });
+
+        },
+        onApprove: (data, actions) => {
+            const { orderID } = data;
+
+            // Capture the order on your server with `orderID`
+
+            return fetch(`/api/paypal/order/${orderID}/capture/`, {
+                method: 'post'
+            }).then((res) => {
+                return res.json();
+            }).then((orderData) => {
+
+                console.log("Payment approved and captured: ", orderData);
+
+                // Handle successful transaction
+
+            }).catch((error) => {
+                
+                // Handle error
+
+            });
+        },
+    });
+
+    const customStyles = {
+        //height: "60px",
+        padding: "10px",
+        fontSize: "18px",
+        fontFamily: '"Open Sans", sans-serif',
+        transition: "all 0.5s ease-out",
+        "input.invalid": {
+            color: "red",
+        },
+    };
+
+    // Optional cardholder name field
+    cardFields.NameField({
+        style: {
+            "input": {
+                "color": "green",
+                "box-shadow": "10px 10px 10px black"
+            },
+            "input.invalid": {
+                "color": "darkgreen"
+            },
+        },
+        onChange: ({isValid, errors}) => {
+            console.log('onchange name: ', isValid, errors);
+        }
+    }).render(cardNameContainer);
+
+    cardFields.NumberField({
+        style: {
+            "input": {
+                "color": "orange",
+                "box-shadow": "10px 10px 10px black"
+            },
+            "input.invalid": {
+                "color": "darkorange"
+            },
+        },
+        onChange: ({isValid, errors}) => {
+            console.log('onchange number: ', isValid, errors);
+        }
+    }).render(cardNumberContainer);
+
+    cardFields.CVVField({
+        style: {
+            "input": {
+                "color": "green",
+                "box-shadow": "10px 10px 10px black"
+            },
+            "input.invalid": {
+                "color": "darkgreen"
+            },
+        },
+        onChange: ({isValid, errors}) => {
+            console.log('onchange cvv: ', isValid, errors);
+        }
+    }).render(cardCvvContainer);
+
+    cardFields.ExpiryField({
+        style: {
+            "input": {
+                "color": "orange",
+                "box-shadow": "10px 10px 10px black"
+            },
+            "input.invalid": {
+                "color": "darkorange"
+            },
+        },
+        onChange: ({isValid, errors}) => {
+            console.log('onchange expiry: ', isValid, errors);
+        }
+    }).render(cardExpiryContainer);
+
+    cardFields.PostalCodeField({
+        style: {
+            "input": {
+                "color": "green",
+                "box-shadow": "10px 10px 10px black"
+            },
+            "input.invalid": {
+                "color": "darkgreen"
+            },
+        },
+        minLength: 5,
+        maxLength: 8,
+        onChange: ({isValid, errors}) => {
+            console.log('onchange postal code: ', isValid, errors);
+        }
+    }).render(cardPostalCodeContainer);
+
+    button.addEventListener('click', () => {
+        console.log('click');
+        cardFields.submit().then(() => {
+
+            // Success
+
+        }).catch((error) => {
+
+            // Handle error
+            console.error(error);
+
+        });
+    });
+
+})();
+
--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const { resolve } = require("path")
 
+//const CLIENT_ID = 'alc_client1'
+//const APP_SECRET = 'secret'
+
+//const CLIENT_ID = 'B_AF7iNYDLbxkdU12xnJWgVZDmfKv7khjdgZ6mMG89YNVD2mH3dE-ypPQxh3WW8eDYSUJUOY0i__WTicdo';
+//const APP_SECRET = 'ENzQHG7oxCna3MsFKqtSx1Vr1fyRIHPfFtnZ4efurqYARwOttdIB_MRslf45l_DA4I1MQz8afW6SFAQf';
+
+// Walter's (sandbox)
+//const CLIENT_ID = 'Ae96woToTpFrWc7YGLd6aPIiKow5-Dy8WidBXtsKYSWe9m-FylfHLn8wKIDWuFrBl6U977n3oDF580O9';
+//const APP_SECRET = 'EMsC5xl047UlwhohCfRpzvUK4hgevX6ndG1GULYiKasPBWkE2P1xpKlhTu10ye-csRewTngMPunAWc1v';
+
+//const CLIENT_ID = 'B_AVLQBSM6Hw4ohRJaEuZGmG6RLKpTAHQw2oyqOEkGOHQbwzwlPpBJRF3cd2MjFegYWHUg0b5GrZRwdOIQ';
+//const APP_SECRET = 'EHOqVUZDLkIcmtWZBKknVQSi5z8E_NtK2qnKNy6bQ-2T_j7N06C8PqZo1aRs1nhgZXTbD2YP3FofUNba';
+
+const CLIENT_ID = 'B_A3Nk8jo-3tRnmAWKP1vmp7hhd4oSTLUSzyS3A3WvmOPWc2syHRjoA68GmQpjiPVc3X2sexAwMQaNn2Lk';
+const APP_SECRET = 'EEF_V0V35vFQl_M7nDSDLBevD_ptRnRmcgZrRBlpac7Vpdz9J0_aCDa5pINFlOXMwe1vfXkccQeUm-uY';
+
+const base = "http://localhost.paypal.com:8000";
+
 const app = express()
 
 app.use(express.static(resolve(__dirname, "../client")))
@@ -8,5 +26,69 @@ app.use(express.static(resolve(__dirname, "../client")))
 app.get("/", (req, res) => {
   res.sendFile(resolve(__dirname, "../client/index.html"));
 });
+
+(async function () {
+
+  async function generateAccessToken() {
+    const auth = Buffer.from(CLIENT_ID + ":" + APP_SECRET).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "post",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+    const data = await response.json();
+    return data.access_token;
+  }
+
+  app.post("/api/paypal/orders/create/", async (req, res) => {
+    // res.sendFile(resolve(__dirname, "../client/index.html"));
+    // console.log('Request', req)
+    // res.json({id: '123456789'})
+    // paypalSDK.createOrder(res)
+    console.log("Response", res.body)
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: "100.00",
+            },
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    console.log('Data: ', data)
+    res.json(data);
+  });
+
+  app.post("/api/paypal/orders/:id/capture", async (req, res) => {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${req.params.id}/capture`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({})
+    });
+    const data = await response.json();
+    console.log('Data: ', data)
+    res.json(data);
+  });
+
+})();
 
 app.listen(8080)
